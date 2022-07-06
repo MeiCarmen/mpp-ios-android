6c2738448895ea654b263f066a5ae17db9dcf24c
Harry Best <harrybest800@gmail.com>
2022-07-06 16:35:25 +0100

    Fix timeformat string

--- SharedCode/src/commonMain/kotlin/DateTimeTools.kt
+++ SharedCode/src/commonMain/kotlin/DateTimeTools.kt
@@ -6,18 +6,14 @@ import com.soywiz.klock.parse
 
 
 const val queryOffsetInSeconds = 6 * 60
-const val apiDateTimeFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS"
+const val apiDateTimeFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX"
 
-fun apiTimeToDateTime(apiTime: String): DateTime {
-    val localTimeFormat = DateFormat(apiDateTimeFormat)
-    val timeWithoutTimeZone = apiTime.split("+").first()
-    val localTime = localTimeFormat.parse(timeWithoutTimeZone)
-    return localTime.local
-}
+
+fun apiTimeToDateTime(apiTime: String) = DateFormat(apiDateTimeFormat).parse(apiTime).local
 
 fun extractSimpleTime(time: String) = apiTimeToDateTime(time).toString("HH:mm")
 
 fun convertToHoursAndMinutes(minutes: Int) = "${minutes / 60}h ${minutes % 60}min"
 
 fun getEarliestSearchableTime() =
-    (DateTime.now().add(0, queryOffsetInSeconds * 1000.0)).toString(apiDateTimeFormat) + "+00:00"
+    DateTime.now().add(0, queryOffsetInSeconds * 1000.0).local.toString(apiDateTimeFormat)
